8eaf0fd343ea13e45d6297986a7f8ba626a672ed
stone <335612970@qq.com>
2020-07-07 19:09:35 +0800

    bloom filter

---
+++ bloomfilter/bloomfilter.go
@@ -0,0 +1,59 @@
+package bloomfilter
+
+import (
+	"hash"
+	"hash/fnv"
+
+	"github.com/spaolacci/murmur3"
+)
+
+type BloomFilter struct {
+	Bytes []byte
+	Hash  []hash.Hash64
+}
+
+func New() *BloomFilter {
+	bf := &BloomFilter{
+		Bytes: make([]byte, 32),
+		Hash: []hash.Hash64{
+			fnv.New64(),
+			murmur3.New64(),
+		},
+	}
+	return bf
+}
+
+// Push 向 BloomFilter 添加元素
+func (bf *BloomFilter) Push(str string) {
+	for _, h := range bf.Hash {
+		// 重置种子
+		h.Reset()
+		// 存入值
+		_, _ = h.Write([]byte(str))
+		// 返回 hash
+		sum := h.Sum64()
+		// 行/列
+		x, y := sum%8, sum%uint64(len(bf.Bytes))
+		// 这么写只适用于 Intel 小端模式，大端模式会有问题
+		bf.Bytes[y] |= 1 << x
+	}
+}
+
+// Exists 判定元素是否可能存在, 返回 false 一定不存在, 返回 true 可能存在
+func (bf *BloomFilter) Exists(str string) bool {
+	for _, h := range bf.Hash {
+		// 重置种子
+		h.Reset()
+		// 存入值
+		_, _ = h.Write([]byte(str))
+		// 返回 hash
+		sum := h.Sum64()
+		// 行/列
+		x, y := sum%8, sum%uint64(len(bf.Bytes))
+		// 这么写只适用于 Intel 小端模式，大端模式会有问题
+		if bf.Bytes[y]|1<<x != bf.Bytes[y] {
+			return false
+		}
+	}
+	return true
+}
---
+++ bloomfilter/bloomfilter_test.go
@@ -0,0 +1,44 @@
+package bloomfilter
+
+import (
+	"testing"
+)
+
+func TestBloomFilter_Exists(t *testing.T) {
+	bf := New()
+	bf.Push("a")
+	bf.Push("b")
+	bf.Push("c")
+	bf.Push("d")
+	bf.Push("e")
+	bf.Push("f")
+	bf.Push("g")
+	bf.Push("h")
+
+	t.Logf("a exists? %v\n", bf.Exists("a"))
+	t.Logf("b exists? %v\n", bf.Exists("b"))
+	t.Logf("c exists? %v\n", bf.Exists("c"))
+	t.Logf("d exists? %v\n", bf.Exists("d"))
+	t.Logf("e exists? %v\n", bf.Exists("e"))
+	t.Logf("f exists? %v\n", bf.Exists("f"))
+	t.Logf("g exists? %v\n", bf.Exists("g"))
+	t.Logf("h exists? %v\n", bf.Exists("h"))
+	t.Logf("i exists? %v\n", bf.Exists("i"))
+	t.Logf("j exists? %v\n", bf.Exists("j"))
+	t.Logf("k exists? %v\n", bf.Exists("k"))
+	t.Logf("l exists? %v\n", bf.Exists("l"))
+	t.Logf("m exists? %v\n", bf.Exists("m"))
+	t.Logf("n exists? %v\n", bf.Exists("n"))
+	t.Logf("o exists? %v\n", bf.Exists("o"))
+	t.Logf("p exists? %v\n", bf.Exists("p"))
+	t.Logf("q exists? %v\n", bf.Exists("q"))
+	t.Logf("r exists? %v\n", bf.Exists("r"))
+	t.Logf("s exists? %v\n", bf.Exists("s"))
+	t.Logf("t exists? %v\n", bf.Exists("t"))
+	t.Logf("u exists? %v\n", bf.Exists("u"))
+	t.Logf("v exists? %v\n", bf.Exists("v"))
+	t.Logf("w exists? %v\n", bf.Exists("w"))
+	t.Logf("x exists? %v\n", bf.Exists("x"))
+	t.Logf("y exists? %v\n", bf.Exists("y"))
+	t.Logf("z exists? %v\n", bf.Exists("z"))
+}
